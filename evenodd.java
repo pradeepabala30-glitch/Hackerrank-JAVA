74caa7c1a87184812b0edaf1701aefc079db9e0e
pradeepabala30-glitch <pradeepabala30@gmail.com>
2026-07-11 21:31:46 +0530

    even or odd

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+//Write a program to print whether a number is even or odd, also take input from the user
+import java.util.Scanner;
+class evenodd
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number:");
+        int num = sc.nextInt();
+        if(num % 2 ==0)
+        {System.out.println(num+"is even number");}
+        else
+        {System.out.println(num+"is odd number");}
+    }
+}
