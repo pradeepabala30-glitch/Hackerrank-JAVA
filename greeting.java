b16d8f4bb799b06e1b714cc459f881299197413d
pradeepabala30-glitch <pradeepabala30@gmail.com>
2026-07-12 22:29:40 +0530

    greeting message

---
+++ greeting.java
@@ -0,0 +1,10 @@
+//Take name as input and print a greeting message for that particular name.
+import java.util.Scanner;
+class greeting{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in) ;
+        String name= sc.nextLine();
+        {
+        System.out.println("hello"+name);
+    }
+}}
